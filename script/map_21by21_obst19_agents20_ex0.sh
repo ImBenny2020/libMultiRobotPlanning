e48f5c734067253ecef0db8fb4e99efad5491b1b
ImBenny2020 <eggborn88@gmail.com>
2021-07-31 15:38:09 +0800

    Add my own maps and scenarios, as well as a bash script for running CBS.

---
+++ script/map_21by21_obst19_agents20_ex0.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+echo "Start to run a simulation of CBS...\nPlease wait or press CTRL-Z to exit."
+
+# inputYAML = "../benchmark/rooms64/map_32by32_obst341_agents5_ex0.yaml"
+# outputYAML = "../build/output.yaml"
+
+../build/cbs -i ../benchmark/gaps2/map_21by21_obst19_agents20_ex0.yaml -o ../build/output.yaml
+
+python3 ../example/visualize.py ../benchmark/gaps2/map_21by21_obst19_agents20_ex0.yaml ../build/output.yaml
+
